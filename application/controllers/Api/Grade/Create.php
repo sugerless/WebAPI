e851953f789b55a3699da1bc15e76b880bd8b254
Suger <601480201@qq.com>
2018-02-06 16:44:15 +0800

    begin

---
+++ application/controllers/Api/Grade/Create.php
@@ -0,0 +1,25 @@
+<?php
+
+
+class Api_Grade_Create_Controller extends Api_Base_Controller
+{
+
+    protected function rules(): array
+    {
+        // TODO: Implement rules() method.
+        return [
+        ];
+    }
+
+    protected function messages(): array
+    {
+        // TODO: Implement messages() method.
+        return [];
+    }
+
+    protected function process()
+    {
+        // TODO: Implement process() method.
+        echo "hello Create";
+    }
+}
---
+++ application/models/Dao/Score.php
@@ -0,0 +1,9 @@
+<?php
+
+
+class Dao_Score_Model extends Dao_Base_Model
+{
+    protected $table='scores';
+    protected $primaryKey='record_id';
+
+}
